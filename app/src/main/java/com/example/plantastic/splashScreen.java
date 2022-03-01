c5e28f546bb71ffdf4aca491e44fbf72332223b6
Daniël <d.a.v.ziel@student.tue.nl>
2022-03-01 11:36:42 +0100

    Added animation for splashscreen

--- app/src/main/java/com/example/plantastic/splashScreen.java
+++ app/src/main/java/com/example/plantastic/splashScreen.java
@@ -3,12 +3,32 @@ package com.example.plantastic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splashScreen extends AppCompatActivity {
+
+    Animation topAnimation, bottomAnimation;
+    ImageView image;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        name = findViewById(R.id.name);
+
+        image.setAnimation(topAnimation);
+        name.setAnimation(bottomAnimation);
+
     }
 }
