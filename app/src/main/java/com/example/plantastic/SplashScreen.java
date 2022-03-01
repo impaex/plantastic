8b57ccac94b5412aa55b0951614acc196172d501
Daniël <d.a.v.ziel@student.tue.nl>
2022-03-01 11:43:03 +0100

    Added transition from splashscreen to sign-up screen

--- app/src/main/java/com/example/plantastic/SplashScreen.java
+++ app/src/main/java/com/example/plantastic/SplashScreen.java
@@ -2,18 +2,21 @@ package com.example.plantastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class splashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     Animation topAnimation, bottomAnimation;
     ImageView image;
     TextView name;
+    private static int SPLASH_SCREEN = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,15 @@ public class splashScreen extends AppCompatActivity {
 
         image.setAnimation(topAnimation);
         name.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
 
     }
 }
