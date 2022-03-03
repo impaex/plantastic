6b624040c1410b662ec96a99ab761a65ec834c25
Daniël <d.a.v.ziel@student.tue.nl>
2022-03-03 08:47:51 +0100

    Added logo transition

--- app/src/main/java/com/example/plantastic/SplashScreen.java
+++ app/src/main/java/com/example/plantastic/SplashScreen.java
@@ -2,9 +2,12 @@ package com.example.plantastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -37,8 +40,16 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashScreen.this, SignUp.class);
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View, String>(image, "logo_image");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }
+                else{
                 startActivity(intent);
-                finish();
+                finish();}
             }
         }, SPLASH_SCREEN);
 
