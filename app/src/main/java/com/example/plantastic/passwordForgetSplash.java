1c6c26f3caebb15318b9ccb724e42c81aef24a50
Daniël <d.a.v.ziel@student.tue.nl>
2022-03-17 16:45:51 +0100

    Added forget Password Interface

---
+++ app/src/main/java/com/example/plantastic/passwordForgetSplash.java
@@ -0,0 +1,14 @@
+package com.example.plantastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class passwordForgetSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_forget_splash);
+    }
+}
