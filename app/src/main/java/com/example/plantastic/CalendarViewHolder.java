d27bd54a1f19d0f978227e17ce2e3b03f26f8182
Max <43404248+impaex@users.noreply.github.com>
2022-03-31 02:04:22 +0200

    Added references to new placeholders

--- app/src/main/java/com/example/plantastic/CalendarViewHolder.java
+++ app/src/main/java/com/example/plantastic/CalendarViewHolder.java
@@ -9,17 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
     private final ArrayList<LocalDate> days;
     public final View parentView;
-    public final TextView dayOfMonth;
+    public final TextView dayOfMonth, event1, event2, event3;
     private final CalendarAdapter.onItemListener onItemListener;
 
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.onItemListener onItemListener, ArrayList<LocalDate> days) {
         super(itemView);
         parentView = itemView.findViewById(R.id.parentView);
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        event1 = itemView.findViewById(R.id.eventcell1);
+        event2 = itemView.findViewById(R.id.eventcell2);
+        event3 = itemView.findViewById(R.id.eventcell3);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
         this.days = days;
